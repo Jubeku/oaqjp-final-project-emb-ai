e8579bb5af44bb13ad9eb4a054b9cbecc873942f
Julian-Kuehnert <julian.kuehnert@ibm.com>
2024-11-13 16:46:29 +0300

    fix error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -11,13 +11,12 @@ def emotion_detector(text_to_analyse):
 
     response = requests.post(url, json = myobj, headers=header)
 
-    formatted_response = json.loads(response.text)['emotionPredictions'][0]['emotion']
-
     if response.status_code == 200:
+        formatted_response = json.loads(response.text)['emotionPredictions'][0]['emotion']
         dominant_emotion = max(formatted_response, key=formatted_response.get)
         formatted_response['dominant_emotion'] = dominant_emotion
     elif response.status_code == 400:
-        formatted_response = {k: None for k, _ in formatted_response.items()}
-        formatted_response['dominant_emotion'] = None
+        labels = ["anger", "disgust", "fear", "joy", "sadness", "dominant_emotion"]
+        formatted_response = {k: None for k in labels}
 
     return formatted_response
